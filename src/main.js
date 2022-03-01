307736c4352fc016d0ff273679a44fb2938a5763
Elías Mercado <eliasmerc23@gmail.com>
2022-03-01 20:31:51 -0300

    Se agrega diccionario json para traducir los textos al español.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import 'devextreme/dist/css/dx.common.css';
 import './themes/generated/theme.base.css';
 import './themes/generated/theme.additional.css';
 import Vue from "vue";
+import esMessages from "./utils/es.json";
+import { locale, loadMessages } from "devextreme/localization";
 
 import App from "./App";
 import router from "./router";
@@ -10,5 +12,9 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    loadMessages(esMessages);
+    locale(navigator.language);
+  }
 }).$mount("#app");
