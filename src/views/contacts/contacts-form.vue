9e6e54a90576b36c5ac87e36cc84270e5ac78cf9
Elías Mercado <eliasmerc23@gmail.com>
2022-03-05 00:38:43 -0300

    Creamos la ventana de edicion del elemento y lo integramos con el backend.

--- src/views/contacts/contacts-form.vue
+++ src/views/contacts/contacts-form.vue
@@ -4,21 +4,67 @@
 
     <dx-data-grid
       class="dx-card wide-card"
-      :data-source="dataSource"
+      :data-source="contactsData"
       :show-column-lines="false"
       :show-row-lines="true"
       :show-borders="true"
       :row-alternation-enabled="true"
+      :repaint-changes-only="true"
+      @row-updating="getJsonForUpdate"
     >
       <dx-paging :page-size="10" />
       <dx-pager :show-page-size-selector="true" :show-info="true" />
-      <dx-load-panel enabled="true"/>
       <dx-editing
         :allow-updating="true"
         :allow-deleting="true"
         :allow-adding="true"
+        refresh-mode="full"
         mode="popup"
-      />
+      >
+        <dx-popup />
+        <dx-form>
+          <dx-item
+            :col-count="3"
+            :col-span="2"
+            item-type="group"
+            caption="Datos Básicos"
+          >
+            <dx-item data-field="nombres" />
+            <dx-item data-field="apellidos" />
+            <dx-item data-field="celular" />
+            <dx-item data-field="email" />
+            <dx-item data-field="fechaNacimiento" />
+            <dx-item data-field="idEstadoCivil" />
+            <dx-item data-field="idTipoDocumento" />
+            <dx-item data-field="documento" />
+            <dx-item data-field="direccion" />
+            <dx-item data-field="idCiudad" />
+            <dx-item data-field="idDepartamento" />
+          </dx-item>
+
+          <dx-item
+            :col-count="3"
+            :col-span="2"
+            item-type="group"
+            caption="Datos Laborales"
+          >
+            <dx-item data-field="idActividadEconomica" />
+            <dx-item data-field="nombreEmpresa" />
+            <dx-item data-field="direccionLaboral" />
+            <dx-item data-field="telefonoLaboral" />
+            <dx-item data-field="correoLaboral" />
+          </dx-item>
+
+          <dx-item
+            :col-count="3"
+            :col-span="2"
+            item-type="group"
+            caption="Datos del Contacto"
+          >
+            <dx-item data-field="idPropietario" />
+          </dx-item>
+        </dx-form>
+      </dx-editing>
 
       <dx-column
         :calculate-cell-value="calculateCellValue"
@@ -27,35 +73,14 @@
         :hiding-priority="8"
         :allow-sorting="false"
       />
-
-      <dx-column
-        data-field="idTipoDocumento"
-        caption="Tipo Documento"
-        :allow-sorting="false"
-        :width="150"
-        :hiding-priority="6"
-      >
-        <dx-lookup
-          :data-source="documentsTypes"
-          value-expr="idTipoDocumento"
-          display-expr="tipoDocumento"
-        />
-      </dx-column>
-
-      <dx-column
-        data-field="documento"
-        caption="Documento"
-        :width="150"
-        :hiding-priority="6"
-        :allow-sorting="false"
-      />
-
+      <dx-column data-field="idContacto" :visible="false" />
+      <dx-column data-field="nombres" :visible="false" />
+      <dx-column data-field="apellidos" :visible="false" />
       <dx-column
         data-field="celular"
         caption="Celular"
         :width="120"
         :allow-sorting="false"
-        :hiding-priority="7"
         :allowFiltering="false"
       />
 
@@ -63,16 +88,52 @@
         data-field="email"
         caption="Email"
         :width="250"
-        :hiding-priority="4"
         :allowFiltering="false"
         :allow-sorting="false"
       />
+      <dx-column
+        data-field="fechaNacimiento"
+        data-type="date"
+        :visible="false"
+      />
+      <dx-column data-field="idEstadoCivil" :visible="false" />
+      <dx-column
+        data-field="idTipoDocumento"
+        caption="Tipo Documento"
+        :allow-sorting="false"
+        :width="150"
+      >
+      </dx-column>
+      <!--    <dx-column
+        data-field="idTipoDocumento"
+        caption="Tipo Documento"
+        :allow-sorting="false"
+        :width="150"
+      >
+        <dx-lookup
+          :data-source="documentsTypes"
+          value-expr="idTipoDocumento"
+          display-expr="tipoDocumento"
+        />
+      </dx-column> -->
 
       <dx-column
-        data-field="propietario"
+        data-field="documento"
+        caption="Documento"
+        :width="150"
+        :allow-sorting="false"
+      />
+      <dx-column data-field="direccion" :visible="false" />
+      <dx-column data-field="idCiudad" :visible="false" />
+      <dx-column data-field="idDepartamento" :visible="false" />
+      <dx-column data-field="idActividadEconomica" :visible="false" />
+      <dx-column data-field="nombreEmpresa" :visible="false" />
+      <dx-column data-field="direccionLaboral" :visible="false" />
+      <dx-column data-field="telefonoLaboral" :visible="false" />
+      <dx-column data-field="correoLaboral" :visible="false" />
+      <dx-column
+        data-field="idPropietario"
         caption="Propietario"
-        name="Priority"
-        :hiding-priority="1"
         :allow-sorting="false"
       />
     </dx-data-grid>
@@ -87,49 +148,83 @@ import DxDataGrid, {
   DxPager,
   DxPaging,
   DxEditing,
-  DxItem,
   DxToolbar,
-  DxLoadPanel
+  DxForm,
+  DxPopup,
 } from "devextreme-vue/data-grid";
 
 import api from "@/scripts/api";
 import notify from "devextreme/ui/notify";
-import auth from '@/auth';
+import auth from "@/auth";
+import CustomStore from "devextreme/data/custom_store";
+import { DxItem } from "devextreme-vue/form";
 
 export default {
   data() {
     return {
-      dataSource: [{}],
+      contactsData: new CustomStore({
+        key: "idContacto",
+        load: () => this.sendRequest("/contacto"),
+        insert: (values) => this.sendRequest("/contacto", "POST", values),
+        update: (key, values) =>
+          this.sendRequest(`/contacto/${key}`, "PUT", values),
+        /*remove: (key) => this.sendRequest(`${URL}/DeleteOrder1`, 'DELETE', {
+          key,
+        }), */
+      }),
       documentsTypes: [{}],
+      editedItem: {}
     };
   },
   methods: {
-    loadDataSource() {
+    sendRequest(url, method = "GET", data = {}) {
       let token = auth.getAutorizationToken();
-      api
-        .get("/contacto", { headers: { Authorization: `Bearer ${token}` } })
-        .then((response) => {
-          this.dataSource = response.data.data;
-        })
-        .catch((error) => {
-          notify(error.response.data.error.message, "error", 2000);
-        });
 
-      this.documentsTypes = [
-        {
-          idTipoDocumento: 2,
-          tipoDocumento: "Cedula",
-        },
-      ];
+      if (method === "GET") {
+        return api
+          .get(url, { headers: { Authorization: `Bearer ${token}` } })
+          .then((response) => {
+            return response.data.data;
+          })
+          .catch((error) => {
+            notify(error.response.data.error.message, "error", 2000);
+          });
+      }
+
+      if (method === "POST") {
+        return api
+          .post(url, data, { headers: { Authorization: `Bearer ${token}` } })
+          .then((response) => {
+            return response.data.data;
+          })
+          .catch((error) => {
+            notify(error.response.data.error.message, "error", 2000);
+          });
+      }
+
+      if (method === "PUT") {
+        return api
+          .put(url, data, { headers: { Authorization: `Bearer ${token}` } })
+          .then((response) => {
+            return response.data.data;
+          })
+          .catch((error) => {
+            notify(error.response.data.error.message, "error", 2000);
+          });
+      }
     },
 
     calculateCellValue(data) {
       return [data.nombres, data.apellidos].join(" ");
     },
+
+    getJsonForUpdate(e) {
+      //devextreme solo retorna el valor que se edito, pero para el back se necesita el json completo.
+      //por eso reemplazamos el newData.
+      e.newData = Object.assign({}, e.oldData, e.newData);
+    },
   },
-  created() {
-    this.loadDataSource();
-  },
+  created() {},
   components: {
     DxDataGrid,
     DxColumn,
@@ -140,7 +235,8 @@ export default {
     DxEditing,
     DxToolbar,
     DxItem,
-    DxLoadPanel
+    DxForm,
+    DxPopup,
   },
 };
 </script>
